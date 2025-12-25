d129947be4c076850824aaa8817cfa3cdec2704f
Shrabon Dhar <dharshrabon2004@gmail.com>
2025-12-25 17:48:32 +0530

    Delete exercise-4.py

--- exercise-4.py
+++
@@ -1,9 +0,0 @@
-# Exercise 04: Even or Odd Number
-
-favourite_number = 7  # you can change this number
-
-if favourite_number % 2 == 0:
-    print(f"{favourite_number} is an Even number")
-else:
-    print(f"{favourite_number} is an Odd number")
-
