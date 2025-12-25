1696720f28c815f65950e41df3e7a804300da4c0
Shrabon Dhar <dharshrabon2004@gmail.com>
2025-12-25 15:03:06 +0530

    Add files via upload

---
+++ exercise-8.py
@@ -0,0 +1,12 @@
+# Exercise 08: Traffic light simulator
+
+signal = input("Enter traffic light color (red/yellow/green): ").lower()
+
+if signal == "red":
+    print("STOP!")
+elif signal == "yellow":
+    print("Prepare to stop")
+elif signal == "green":
+    print("You can go")
+else:
+    print("Wrong input!")
