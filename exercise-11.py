9dab1d5ffadea904b85fb36b05ea911274be0cc4
Shrabon Dhar <dharshrabon2004@gmail.com>
2025-12-25 17:09:22 +0530

    Add files via upload

---
+++ exercise-11.py
@@ -0,0 +1,4 @@
+# Exercise 11: Print numbers from 1 to 15
+
+for i in range(1, 16):
+    print(i)
---
+++ exercise-12.py
@@ -0,0 +1,7 @@
+# Exercise 12: Print even numbers from 2 to 30
+
+num = 2
+
+while num <= 30:
+    print(num)
+    num += 2
